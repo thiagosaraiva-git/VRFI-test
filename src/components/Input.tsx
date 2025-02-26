907f1442bcca96f925edc88b2d9ebcbff80efa04
thiagosaraiva-git <saraii_@live.com>
2025-02-26 12:50:16 -0800

    style: Input optional label added

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -5,20 +5,26 @@ import { Eye, EyeOff } from "lucide-react";
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   placeholder: string;
+  label?: string;
   type: "text" | "password";
 }
 
-const Input = ({ placeholder, type = "text", ...props }: InputProps) => {
+const Input = ({ placeholder, label, type = "text", ...props }: InputProps) => {
   const [showPassword, setShowPassword] = useState(false);
 
   return (
     <div className="flex flex-col gap-1 w-full">
       <div className="relative">
+        {label && (
+          <label className="absolute left-3 top-3 text-secondary-dark text-sm">
+            {label}
+          </label>
+        )}
         <input
           {...props}
           type={type === "password" && showPassword ? "text" : type}
-          className="w-full border border-secondary rounded-[20px] text-primary-dark outline-none focus:border-primary-dark p-6 shadow-custom"
-          placeholder={placeholder}
+          className={`w-full border border-secondary rounded-[20px] text-primary-dark outline-none focus:border-primary-dark p-6 shadow-custom ${label && "pt-8"} placeholder:text-secondary-dark`}
+          placeholder={!label ? placeholder : ""}
         />
         {type === "password" && (
           <button
