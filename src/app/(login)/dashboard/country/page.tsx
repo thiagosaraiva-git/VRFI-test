bc84acb7e0b5f20d2965b277fc133b245f2bdce6
thiagosaraiva-git <saraii_@live.com>
2025-02-27 18:24:45 -0800

    feat: responsivity improvements

--- src/app/(login)/dashboard/country/page.tsx
+++ src/app/(login)/dashboard/country/page.tsx
@@ -4,7 +4,7 @@ import { useSearchParams } from "next/navigation";
 import { useCountry } from "@/hooks/useCountry";
 import Image from "next/image";
 
-export default function CountryDetail() {
+export default function Country() {
   const [countryName, setCountryName] = useState<string | null>(null);
   const { country, fetchCountryDetails, loading, error } = useCountry();
   const searchParams = useSearchParams();
@@ -20,7 +20,6 @@ export default function CountryDetail() {
     if (countryName) {
       fetchCountryDetails(countryName);
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [countryName]);
 
   if (loading) return <p>Loading...</p>;
@@ -28,7 +27,7 @@ export default function CountryDetail() {
   if (!country) return <p>Country not found</p>;
 
   return (
-    <div className="mx-auto p-4 md:ml-52 ">
+    <div className="mx-auto p-4 md:ml-52">
       <h1 className="text-[40px] font-bold mb-2 text-primary">
         {country.name.common}
       </h1>
@@ -36,10 +35,10 @@ export default function CountryDetail() {
         A short description about {country.name.common}
       </p>
 
-      <div className="grid grid-cols-3 gap-8 text-secondary">
-        <div className="flex flex-col bg-white rounded-[20px] p-6 shadow-light">
-          <p className="text-[16px] mb-6">Country Flag</p>
-          <div className="overflow-hidden rounded-lg">
+      <div className="grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-6 text-secondary">
+        <div className="flex flex-col bg-white rounded-[20px] p-6 shadow-light h-full">
+          <p className="text-[16px] mb-4">Country Flag</p>
+          <div className="overflow-hidden rounded-lg flex justify-center items-center h-full">
             <Image
               src={country.flags.png}
               alt={`Flag of ${country.name.common}`}
@@ -50,17 +49,17 @@ export default function CountryDetail() {
           </div>
         </div>
 
-        <div className="flex flex-col space-y-6">
-          <div className="bg-white rounded-[20px] p-6 shadow-light">
+        <div className="flex flex-col space-y-4 h-full">
+          <div className="bg-white rounded-[20px] p-6 shadow-light flex-1">
             <p className="text-[16px] mb-2">Population</p>
-            <div className="text-[40px] text-primary">
+            <div className="text-[32px] text-primary truncate">
               {country.population.toLocaleString()}
             </div>
           </div>
 
-          <div className="bg-white rounded-[20px] p-6 shadow-light">
+          <div className="bg-white rounded-[20px] p-6 shadow-light flex-1">
             <p className="text-[16px] mb-2">Language</p>
-            <div className="text-[40px] text-primary">
+            <div className="text-[32px] text-primary truncate">
               {country.languages
                 ? Object.values(country.languages).join(", ")
                 : "N/A"}
@@ -68,17 +67,17 @@ export default function CountryDetail() {
           </div>
         </div>
 
-        <div className="flex flex-col space-y-6">
-          <div className="bg-white rounded-[20px] p-6 shadow-light">
+        <div className="flex flex-col space-y-4 h-full">
+          <div className="bg-white rounded-[20px] p-6 shadow-light flex-1">
             <p className="text-[16px] mb-2">Capital</p>
-            <p className="text-[40px] text-primary">
+            <p className="text-[32px] text-primary truncate">
               {country.capital ? country.capital[0] : "N/A"}
             </p>
           </div>
 
-          <div className="bg-white rounded-[20px] p-6 shadow-light">
+          <div className="bg-white rounded-[20px] p-6 shadow-light flex-1">
             <p className="text-[16px] mb-2">Currency</p>
-            <div className="text-[40px] text-primary">
+            <div className="text-[32px] text-primary truncate">
               {country.currencies
                 ? Object.values(country.currencies)
                     // eslint-disable-next-line @typescript-eslint/no-explicit-any
